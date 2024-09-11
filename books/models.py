f8f2c46d715c82f1249d0d77d4c02e60a286567f
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-11 14:43:39 +0300

    Add title ordering in Book model

--- books/models.py
+++ books/models.py
@@ -12,5 +12,8 @@ class Book(models.Model):
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
 
+    class Meta:
+        ordering = ("title",)
+
     def __str__(self):
         return f"{self.title} - {self.author}"
