c9805e0c01e6ed6bb25eb65df391b08b8ce53c7b
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-12 16:48:31 +0300

    Add BorrowingListSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -7,3 +7,18 @@ class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user",)
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book = serializers.CharField(
+        source="book.title",
+        read_only=True
+    )
+    user = serializers.CharField(
+        source="user.full_name",
+        read_only=True
+    )
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user",)
