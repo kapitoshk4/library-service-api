e89495dbe315d3d398cc9238e6083d6813540a9b
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-12 16:49:01 +0300

    Add get_serializer_class for list in BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.action == "list":
+            serializer_class = BorrowingListSerializer
+
+        return serializer_class
