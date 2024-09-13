79ea481a8175d01718bff6eb2a8b16ed54a980a3
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-13 23:12:53 +0300

    Add is_active & users filter to BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from borrowings.models import Borrowing
@@ -8,6 +9,11 @@ from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer,
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def _params_to_int(query_string):
+        return [int(str_id) for str_id in query_string.split(",")]
 
     def get_serializer_class(self):
         serializer_class = self.serializer_class
@@ -20,6 +26,24 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = self.queryset
+        is_active = self.request.query_params.get("is_active")
+        users = self.request.query_params.get("users")
         queryset = queryset.select_related("user", "book")
 
+        if is_active:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        if users and self.request.user.is_staff:
+            user_ids = self._params_to_int(users)
+            queryset = queryset.filter(user__id__in=user_ids)
+
         return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
