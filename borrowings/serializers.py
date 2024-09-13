2d32cb9ea616cef060efe45de43cbeb0d02f4b84
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-13 21:33:42 +0300

    Add class Meta to BorrowingRetrieveSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -7,7 +7,25 @@ from books.serializer import BookSerializer
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user",)
+        fields = ("id", "book", "expected_return_date",)
+
+    def validate(self, attrs):
+        Borrowing.validate_borrowing(
+            attrs["book"].inventory
+        )
+        return attrs
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+
+        borrowing = Borrowing.objects.create(
+            user=self.context["request"].user,
+            book=book,
+            expected_return_date=validated_data["expected_return_date"]
+        )
+        return borrowing
 
 
 class BorrowingListSerializer(serializers.ModelSerializer):
@@ -26,8 +44,12 @@ class BorrowingListSerializer(serializers.ModelSerializer):
 
 
 class BorrowingRetrieveSerializer(BorrowingSerializer):
-    book = BookSerializer()
+    book = BookSerializer(read_only=True)
     user = serializers.CharField(
         source="user.full_name",
         read_only=True
     )
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user",)
