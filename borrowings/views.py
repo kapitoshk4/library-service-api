244731d5a284ca9b930d646b9b320fa13f985cfd
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-13 23:22:30 +0300

    Remove unnecessary modules in views.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingRetrieveSerializer
