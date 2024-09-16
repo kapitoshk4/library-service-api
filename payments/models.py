c3e59e15223d52776c210670322b171c47d78d48
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-16 12:34:48 +0300

    Add related_names to borrowing field in Payment model

--- payments/models.py
+++ payments/models.py
@@ -14,7 +14,7 @@ class Payment(models.Model):
 
     status = models.CharField(choices=Status.choices, max_length=7)
     type = models.CharField(choices=Type.choices, max_length=7)
-    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=9, decimal_places=2)
