c3e970be93bc9488788b2b63755912612b6d19d0
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-18 21:35:56 +0300

    Add calculate_fines to tasks.py

--- borrowings/tasks.py
+++ borrowings/tasks.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 from borrowings.models import Borrowing
 from borrowings.telegram_helper import send_telegram_message
 from payments.models import Payment
+from payments.utils import create_stripe_payment_session
+
+FINE_MULTIPLAYER = 2
 
 
 @shared_task
@@ -36,4 +39,17 @@ def check_expired_sessions():
         if session["status"] == "expired" or session["expires_at"] < now.timestamp():
             payment.status = "Expired"
             payment.save()
-    
+
+
+@shared_task
+def calculate_fines():
+    today = timezone.now()
+    borrowings = Borrowing.objects.filter(
+        actual_return_date__isnull=True,
+        expected_return_date=today
+    )
+    for borrowing in borrowings:
+        days_overdue = (today - borrowing.expected_return_date).days
+        if days_overdue > 0:
+            fine_amount = days_overdue * borrowing.book.daily_fee * FINE_MULTIPLAYER
+            create_stripe_payment_session(borrowing, current_request=None, payment_type="Fine", total_price=fine_amount)
