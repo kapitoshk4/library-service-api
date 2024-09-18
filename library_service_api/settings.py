864ccc333c73e9a7287db2f20b79a5f289422ddf
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-18 21:35:34 +0300

    Add DEFAULT_PERMISSION_CLASSES & DEFAULT_PAGINATION_CLASS to settings

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -144,7 +144,12 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_RATES": {"anon": "10000/day", "user": "10000/day"},
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5
 }
 
 SIMPLE_JWT = {
