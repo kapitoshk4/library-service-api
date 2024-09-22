373e1998407aa0cf3bfd6d2648bb535784e218c7
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-23 00:15:48 +0300

    Change session_url & session_id max_length to 500 in Payment model

--- payments/models.py
+++ payments/models.py
@@ -16,8 +16,8 @@ class Payment(models.Model):
     status = models.CharField(choices=Status.choices, max_length=7)
     type = models.CharField(choices=Type.choices, max_length=7)
     borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
-    session_url = models.URLField()
-    session_id = models.CharField(max_length=255)
+    session_url = models.URLField(max_length=500)
+    session_id = models.TextField(max_length=500)
     money_to_pay = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __str__(self):
