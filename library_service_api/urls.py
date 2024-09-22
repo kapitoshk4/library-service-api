52d2ff2865a457406ec79c5e68d692f426777ead
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-23 00:14:25 +0300

    Add drf_spectacular urls

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -18,6 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +30,15 @@ urlpatterns = [
     path("api/v1/user/", include("users.urls"), name="users"),
     path("api/v1/borrowing/", include("borrowings.urls"), name="borrowings"),
     path("api/v1/payment/", include("payments.urls"), name="payments"),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+      ),
+    path(
+        "api/v1/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+      ),
 ] + debug_toolbar_urls()
